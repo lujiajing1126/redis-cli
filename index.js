af156a850b6c6f629266e1d5fc1d2030acc3862b
Gregg B. Jensen <greggbjensen@gmail.com>
2018-04-24 13:47:53 -0600

    Added the ability to pass -a for AUTH and added quit application on redis session end.

--- index.js
+++ index.js
@@ -5,21 +5,23 @@ var program = require('commander');
 program.version('1.0.2')
 	.usage("rdcli [OPTIONS] [cmd [arg [arg ...]]]")
 	.option("-h, --host <host>",'Server hostname (default: 127.0.0.1).')
-	.option("-p, --port <port>","Server port (default: 6379).",parseInt)
+	.option("-p, --port <port>","Server port (default: 6379).", parseInt)
+	.option("-a, --auth <password>",'Server password.')
 	.option("-s, --socket <socket>","Server socket (overrides hostname and port).")
 	.option("-m, --mode <mode>","Server Type, only redis available now.")
 	.parse(process.argv);
 
 var host = program.host || "127.0.0.1";
 var port = program.port || 6379;
+var auth = program.auth || null;
 var mode = program.mode || "redis";
 var socket = program.socket;
 if(mode.toLowerCase() == 'redis') {
 	if(socket !== undefined) {
 		require('./lib/redis')(socket);
 	} else {
-		require('./lib/redis')(host,port);
+		require('./lib/redis')(host, port, auth);
 	}
 } else {
-	console.log("Not Support %s Now!",mode);
+	console.log("Not Support %s Now!", mode);
 }
